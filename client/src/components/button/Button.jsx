d73292cb02078e8d1fb18db165a29eb4e06cb819
mohamad <msiysinyuy@gmail.com>
2022-05-09 18:08:37 +0100

    update on the dropdown with icon as well as seting up getting data for svg file

---
+++ client/src/components/button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button>save</button>
+  )
+}
+
+export default Button
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Toldbar} from './toolbar/Toldbar'
 export {default as DropdownMenu} from './dropdown/DropdownMenu'
+export {default as Button} from './button/Button'
 export {default as NumberInput} from './input/NumberInput'
